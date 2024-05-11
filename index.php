2f4ea35ccb59793bce7ede7bd7173bfa57c0115c
pranvera24 <elsahalili84@gmail.com>
2024-05-11 19:33:36 +0200

    dfghjkl;
    dfghjk

--- index.php
+++ index.php
@@ -215,7 +215,7 @@
       <!-- Section Title -->
       <div class="container section-title" data-aos="fade-up">
         <h2>Services</h2>
-        <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
+        <p>We are a eco friendly website trying to help people to save our planet Earth!</p>
       </div><!-- End Section Title -->
 
       <div class="container">
@@ -225,32 +225,32 @@
           <div class="col-xl-3 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="100">
             <div class="service-item position-relative">
               <div class="icon"><i class="bi bi-activity icon"></i></div>
-              <h4><a href="service-details.html" class="stretched-link">Lorem Ipsum</a></h4>
-              <p>Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi</p>
+              <h4><a href="service-details.html" class="stretched-link">Sell Plastic bottles!</a></h4>
+              <p>Sell your used plastic bottles</p>
             </div>
           </div><!-- End Service Item -->
 
           <div class="col-xl-3 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="200">
             <div class="service-item position-relative">
               <div class="icon"><i class="bi bi-bounding-box-circles icon"></i></div>
-              <h4><a href="service-details.html" class="stretched-link">Sed ut perspici</a></h4>
-              <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore</p>
+              <h4><a href="service-details.html" class="stretched-link">Report people polluting the Earth</a></h4>
+              <p>Take pictures and repost to us if u see someone polluting our planet Earth and report it to us!</p>
             </div>
           </div><!-- End Service Item -->
 
           <div class="col-xl-3 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="300">
             <div class="service-item position-relative">
               <div class="icon"><i class="bi bi-calendar4-week icon"></i></div>
-              <h4><a href="service-details.html" class="stretched-link">Magni Dolores</a></h4>
-              <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia</p>
+              <h4><a href="service-details.html" class="stretched-link">We recycle glass!</a></h4>
+              <p>Give us unded glass pieces!</p>
             </div>
           </div><!-- End Service Item -->
 
           <div class="col-xl-3 col-md-6 d-flex" data-aos="fade-up" data-aos-delay="400">
             <div class="service-item position-relative">
               <div class="icon"><i class="bi bi-broadcast icon"></i></div>
-              <h4><a href="service-details.html" class="stretched-link">Nemo Enim</a></h4>
-              <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+              <h4><a href="service-details.html" class="stretched-link">We collect platic from the ocean!</a></h4>
+              <p>Be a part of our team and help us collect unwanted pieces of plastic from the ocean, and save the turtles!</p>
             </div>
           </div><!-- End Service Item -->
 
